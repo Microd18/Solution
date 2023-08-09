ac795f7d9e9d9ec4b051b39f669d0696c6d55f96
Microd <microd@inbox.ru>
2023-08-09 16:46:20 +0500

    Third commit

--- src/Solution.java
+++ src/Solution.java
@@ -2,7 +2,20 @@ import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(1);
+        System.out.println(Arrays.toString(splitString("Viktor Ivanov Ivan Petrov", ' ')));
+    }
+
+    public static String[] trimString(String str) {
+        String[] result = new String[2];
+
+        result[0] = str.substring(0, str.length()/2);
+        result[1] = str.substring(str.length()/2);
+
+        return result;
+    }
+
+    public static String[] splitString(String str, char ch) {
+        return str.split(String.valueOf(ch));
     }
 
 }
